fe3442229f6c801bc34524089935a7055b1002c6
SamuraiJack <75142043+Tullu88@users.noreply.github.com>
2021-07-11 10:56:37 +0200

    resources to make website live

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import logo from './images/Logo-Black.png';
 
 function App() {
   return (
     <div className="App">
-      <div>Hello World</div>
+      <img src={logo} alt="logo" className="logo"/>
+      <h2>Barcelona based startup working on making learning a fun experience with the goal of reducing school dropout.</h2>
+      <h3>Website under construction</h3>
     </div>
   );
 }
